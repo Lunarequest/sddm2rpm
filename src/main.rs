e3148d0daf180e0577addfffe30ffd16f58516b2
Luna <advaith.madhukar@gmail.com>
2021-08-27 12:42:30 +0530

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use clap::{App, Arg};
+mod archive;
+mod rpm_build;
+
+fn name_from_file(filename: &String) -> String {
+    return filename.to_owned().replace("tar.gz", "")
+}
+
+fn main() {
+    let matches = App::new("sddm2rpm")
+                    .version("0.1.0")
+                    .about("takes sddm theme as tar.gz files and repacks them to rpms")
+                    .arg(Arg::with_name("source")
+                        .required(true)
+                        .help("path to sddm archive").index(1))
+                    .arg(Arg::with_name("dest")
+                        .help("directory to unpack too").index(2))
+                    .get_matches();
+    let source = matches.value_of("source").unwrap().to_owned().to_string();
+    let dest = matches.value_of("dest").unwrap_or("./unpacked").to_owned().to_string();
+    // always clean up after yourself
+    match archive::unpack(&source, &dest) {
+        Ok(()) => {
+            let name = name_from_file(&source);
+            rpm_build::buildrpm(&dest, name);
+            archive::cleanup(&dest);
+        },
+        Err(()) => {
+            archive::cleanup(&dest);
+        }
+    }
+}
---
+++ src/rpm_build.rs
@@ -0,0 +1,18 @@
+use walkdir;
+use std::env;
+use std::str::FromStr;
+use std::path::Path;
+use rpm;
+
+pub fn buildrpm(source: &String, name: String) {
+    let current_dir = env::current_dir().unwrap();
+    let wd = Path::new(source);
+    assert!(env::set_current_dir(wd).is_ok());
+    // rpm-rs handles setting up the compressor lets use it
+    let pkg = rpm::RPMBuilder::new(name.as_str(), "1.0.0", "GPL","noarch", "autogenrated rpms")
+        .compression(rpm::Compressor::from_str("gzip").unwrap());
+    for entry in walkdir::WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+        println!("{}", entry.path().display());
+    }
+    assert!(env::set_current_dir(current_dir).is_ok());
+}
