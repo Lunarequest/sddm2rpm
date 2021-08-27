807218ddcffcf6cdd056f467de8ed4cd6c89af26
Luna <advaith.madhukar@gmail.com>
2021-08-27 13:15:01 +0530

    formated source code

--- src/archive.rs
+++ src/archive.rs
@@ -1,21 +1,21 @@
-use std::path::Path;
-use std::fs::{File, remove_dir_all};
 use flate2::read::GzDecoder;
+use std::fs::{remove_dir_all, File};
+use std::path::Path;
 use tar::Archive;
 
-pub fn unpack(source: &String, dest: &String) -> Result<(),()> {
+pub fn unpack(source: &String, dest: &String) -> Result<(), ()> {
     let source_path = Path::new(source);
     if source_path.exists() && source_path.is_file() {
         let tar_bin = File::open(source).unwrap();
         let tar = GzDecoder::new(tar_bin);
         let mut archive = Archive::new(tar);
         archive.unpack(dest).unwrap();
-        return Ok(())
-    }else{
+        return Ok(());
+    } else {
         panic!("Path is not valid")
     }
 }
-    
+
 pub fn cleanup(path: &String) {
     remove_dir_all(path).unwrap();
 }
